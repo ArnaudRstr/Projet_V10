ee4ab13aff4a6f432b46f7431d7e22e6668bdb32
arnaud <arnaud@XPS-13-Arnaud>
2023-12-09 18:50:09 +0100

    Merge origin/main into main
    Conflicts:
    	src/main/java/fr/insa/rastetter/mainview/Entete.java
    	src/main/java/fr/insa/rastetter/mainview/FenetrePartagee.java
    	src/main/java/fr/insa/rastetter/mainview/VuePrincipale.java

--- src/main/java/fr/insa/rastetter/mainview/Entete.java
+++ src/main/java/fr/insa/rastetter/mainview/Entete.java
@@ -6,12 +6,13 @@ package fr.insa.rastetter.mainview;
 
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.contextmenu.ContextMenu;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.contextmenu.SubMenu;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.notification.Notification;
+import java.sql.Connection;
 /**
  *
  * @author arnaud
@@ -22,13 +23,38 @@ public class Entete extends MyHorizontalLayout {
     
     
     
-    private Button CompteUtilisateur;
-    private MenuBar menuBar = new MenuBar();
-    private MenuBar menuFichier = new MenuBar();
+    
+    
+    //Boutons
+    private Button boutonCompteUtilisateur;
+    
+    
+    
+    
+    
+    
+    
+    
+    private MenuBar menuBar;
+    private MenuBar menuFichier;
+    private MenuItem menuItemMenu;
+    
+    
+    
+    //Pour le bouton de menu principal
+    private SubMenu subMenuMenuPrincipal;
+    private MenuItem menuItemPlan;
+    private MenuItem menuItemMachine;
+    private MenuItem menuItemProduit;
+    
+    private MenuItem menuItemAtelier;
+    private SubMenu subMenuAtelier;
+    private MenuItem menuItemAjouterAtelier;
     
     
     public Entete(VuePrincipale main) {
         this.main = main;
+        
         this.addClassName("Custom-Entete");
         //this.getStyle().set("border", "1px solid #000000");
         this.getStyle().set("padding", "10px");
@@ -44,12 +70,44 @@ public class Entete extends MyHorizontalLayout {
         
         
         //Configuration de la menubar Menu
-        MenuItem menu = menuBar.addItem("Menu", e -> {
+        this.menuBar = new MenuBar();
+        this.add(this.menuBar);
+        
+                
+        this.menuItemMenu= this.menuBar.addItem("Menu Principal");
+        
+        //On crée un sous menu du menu item du menu principal
+        this.subMenuMenuPrincipal= menuItemMenu.getSubMenu();
+        
+        //On ajoute les menu item au sous menu du menuitem du menu principal
+        this.menuItemPlan = this.subMenuMenuPrincipal.addItem("Plan");
+        this.menuItemMachine = this.subMenuMenuPrincipal.addItem("Machines");
+        this.menuItemProduit = this.subMenuMenuPrincipal.addItem("Produits");
+
+        
+        this.menuItemAtelier=this.menuBar.addItem("Atelier");
+        this.subMenuAtelier=menuItemAtelier.getSubMenu();
+        
+        
+        this.menuItemAjouterAtelier=this.subMenuAtelier.addItem("Ajouter");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+     /*           addItem("Menu", e -> {
     // Code à exécuter lors du clic sur le MenuItem
     Notification.show("MenuItem cliqué !");
     });
         
-
+     
+     
+     
         SubMenu Plan = menu.getSubMenu();
         Plan.addItem("Plan", e -> {
     
@@ -72,7 +130,7 @@ public class Entete extends MyHorizontalLayout {
     Notification.show("Produit cliqué !");
     });
         
-        
+       
         
         
         
@@ -97,23 +155,83 @@ public class Entete extends MyHorizontalLayout {
     Notification.show("Supprimer");
  
     });
-        
+    */
         
         
         
     
         //menuBar.addItem(menu);
-        this.add(menuBar);
-        this.add(menuFichier);
+        //this.add(menuBar);
+        //this.add(menuFichier);
         
         this.add(new H1("ça c'est l'entête"));
 
         
-        CompteUtilisateur = new Button("Compte");
-        this.add(CompteUtilisateur);
+        //CompteUtilisateur = new Button("Compte");
+        //this.add(CompteUtilisateur);
         
         //this.add(new Paragraph("merci de vous connecter"));
         
+        
+        
+        
+        //On crée et on ajoute les boutons
+        this.boutonCompteUtilisateur = new Button("Compte");
+        this.add(boutonCompteUtilisateur);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //On renvoie l'événement vers le controleur de la vue principale
+        boutonCompteUtilisateur.addClickListener(event -> {
+            Notification.show("Option Compte sélectionnée !");
+            this.main.getControleur().boutonCompte();
+        });
+        
+        
+        
+        
+        menuItemPlan.addClickListener(event -> {
+            Notification.show("Option Plan sélectionnée !");
+            this.main.getControleur().MenuItemPlan();
+        });
+        
+        
+        menuItemMachine.addClickListener(event -> {
+            Notification.show("Option machine sélectionnée !");
+            this.main.getControleur().MenuItemMachine();
+        });
+        
+        
+        
+        menuItemProduit.addClickListener(event -> {
+            Notification.show("Option produit sélectionnée !");
+            this.main.getControleur().MenuItemProduit();
+        });
+        
+        
+        menuItemAjouterAtelier.addClickListener(event -> {
+            Notification.show("Option produit sélectionnée !");
+            this.main.getControleur().MenuItemAjouterAtelier();
+            
+            
+            
+            
+        });
+
 }
     
     
