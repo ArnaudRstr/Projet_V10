20668048e75cf5aa0c6e7d9cdde4e839e5e2123d
molys <molys@172.29.95.97>
2024-01-14 01:37:36 +0100

    plan

--- src/main/java/fr/insa/rastetter/mainview/VuePlan.java
+++ src/main/java/fr/insa/rastetter/mainview/VuePlan.java
@@ -5,6 +5,7 @@
 package fr.insa.rastetter.mainview;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
@@ -18,6 +19,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -31,27 +34,91 @@ public class VuePlan extends MyVerticalLayout {
     private ArrayList<PlanButton> spaceMachineTaked;
     private ArrayList<PlanButton> spaceSelect;
     private ArrayList<Machine> listMachine;
+    private Machine machinecbb;
+
+    public Atelier getAtelier() {
+        return atelier;
+    }
+
+    public int getOrdonnee() {
+        return ordonnee;
+    }
+
+    public int getAbscisse() {
+        return abscisse;
+    }
+
+    public ArrayList<PlanButton> getSpaceMachineTaked() {
+        return spaceMachineTaked;
+    }
+
+    public ArrayList<PlanButton> getSpaceSelect() {
+        return spaceSelect;
+    }
+
+    public ArrayList<Machine> getListMachine() {
+        return listMachine;
+    }
+
+    public Machine getMachinecbb() {
+        return machinecbb;
+    }
+    
     
     public VuePlan(Controleur controleur,int idatelier)throws SQLException {
-//        Atelier atelier = new Atelier(controleur.getVuePrincipale().getGestionBDD().conn, idatelier);
-//        this.atelier = atelier;
-        this.ordonnee = 10;
-        this.abscisse=15;
+        System.out.println("VuePlan");
+        Atelier atelier = new Atelier(controleur.getVuePrincipale().getGestionBDD().conn, idatelier);
+        System.out.println("Atelier trouvé");
+        this.atelier = atelier;
+        this.ordonnee = atelier.getLargeur();
+        this.abscisse= atelier.getLongueur();
         this.spaceSelect= new ArrayList();
         this.spaceMachineTaked= new ArrayList();
-//        this.listMachine = GestionBDD.listMachineAtelier(controleur.getVuePrincipale().getGestionBDD().conn,atelier.getId());
+        this.listMachine = GestionBDD.listMachineAtelier(controleur.getVuePrincipale().getGestionBDD().conn,atelier.getId());
+        System.out.println("les attributs sont initialisés");
+        
+        //Combobox Machine
+        ComboBox<Machine> cbbMachine = new ComboBox();
+        cbbMachine.setItems(this.listMachine);
+        cbbMachine.setItemLabelGenerator(Machine::getNom);
+        cbbMachine.setReadOnly(false);
+        cbbMachine.setLabel("Machine");
+        this.machinecbb=cbbMachine.getValue();
+        
+        //Bouton valider
         Button valider = new Button("Valider");
         valider.addClickListener(even -> {
+            try {
+                GestionBDD.deletePlacementMachine(controleur.getVuePrincipale().getGestionBDD().conn, cbbMachine.getValue());
+                for (int i = 0; i<spaceSelect.size();i++){
+                    GestionBDD.addPlacementMachine(controleur.getVuePrincipale().getGestionBDD().conn, cbbMachine.getValue(), spaceSelect.get(i).getAbscisse(), spaceSelect.get(i).getOrdonnee());
+                }
+                
+            } catch (SQLException ex) {
+                Logger.getLogger(VuePlan.class.getName()).log(Level.SEVERE, null, ex);
+            }
             for (int i=0; i<spaceSelect.size();i++){
                 spaceMachineTaked.add(spaceSelect.get(i));
             }
+            
+            spaceSelect= new ArrayList();
+            
             for (int i=0; i<spaceMachineTaked.size();i++){
                 spaceMachineTaked.get(i).getStyle().set("background-color", "grey");
             }
+            
         });
+        
+        
+        
+        //Colonne des boutons de base
+        HorizontalLayout hltouls = new HorizontalLayout();
+        hltouls.add(valider);
+        hltouls.add(cbbMachine);
+        
+        //Plan
         VerticalLayout vlOrdonnee = new VerticalLayout();
        
-        
         for (int i=0;i<this.ordonnee;i++){
             vlOrdonnee.add(new H2(String.valueOf(i)));
         }
@@ -59,7 +126,7 @@ public class VuePlan extends MyVerticalLayout {
         this.add(new H2("Plan de l'atelier"));
         
         HorizontalLayout hl = new HorizontalLayout();
-        hl.add(valider);
+        hl.add(hltouls);
         hl.add(vlOrdonnee);
         for (int i=0;i<abscisse;i++ ){
             hl.add(createVerticalLayout(i));
@@ -73,6 +140,7 @@ public class VuePlan extends MyVerticalLayout {
     private String changeButtonColor(PlanButton button){
       // Obtenez la couleur actuelle du bouton
         String currentColor = button.getStyle().get("background-color");
+        
 
         // Utilisez une instruction if pour vérifier la couleur actuelle et changer en conséquence
         if ("grey".equals(currentColor)){
@@ -97,10 +165,11 @@ public class VuePlan extends MyVerticalLayout {
         
         VerticalLayout vl = new VerticalLayout();
         vl.setSpacing(false);
+        vl.setWidth("30px");
         vl.add(new H2(String.valueOf(abscisse)));
         for (int j=0;j<ordonnee;j++){
           
-          vl.add(new PlanButton(abscisse,j));
+          vl.add(new PlanButton(abscisse,j,this.machinecbb));
         }
         return vl;
     }
@@ -112,13 +181,15 @@ public class VuePlan extends MyVerticalLayout {
         private int ordonnee;
         private String color;
 
-        public PlanButton(int abscisse,int ordonnee){
+        public PlanButton(int abscisse,int ordonnee,Machine cbbMachine){
             this.abscisse=abscisse;
             this.ordonnee=ordonnee;
+            
             this.getStyle().setBackground("white");
+            
             this.color=this.getStyle().get("background-color");
-            this.setWidth("50px");
-            this.setHeight("50px");
+            this.setWidth("25px");
+            this.setHeight("25px");
             this.addClickListener(even -> {
 
                 this.color=changeButtonColor(this);
